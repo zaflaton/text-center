40274198d41e5533e7c507bb8b60f278030bb40c
zaflaton <59338890+zaflaton@users.noreply.github.com>
2023-12-06 09:29:53 +0800

    created client section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,23 +1,258 @@
 import TitleSection from '@/components/landing-page/title-section'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import Image from 'next/image'
+import Banner from '../../../public/appBanner.png'
+import { CLIENTS } from '@/lib/constants'
 
 export default function HomePage() {
   return (
-    <section>
-      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+    <>
+      <section className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
         <TitleSection
           pill="🌟 Your Workspace , Perfected"
           title="All-In-One Collaboration and Productivity Platform"
         />
-        <div className="p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-sky-600">
+        <div className="p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-secondary z-10">
           <Button
             variant="secondary"
-            className="w-full rounded-xl p-6 text-2xl bg-background">
+            className="w-full rounded-[10px] p-6 text-2xl bg-background z-10">
             Get TextCenter Free Now!
           </Button>
         </div>
-      </div>
-    </section>
+        <div className="relative md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-30px] ">
+          <Image src={Banner} alt="Banner" />
+          {/* <div className="absolute top-1/2 right-0 bottom-0 left-0 z-10 bg-gradient-to-t from-background"></div> */}
+        </div>
+      </section>
+      <section className="relative">
+        <div className='after:absolute overflow-hidden flex after:content[""] after:to-transparent after:from-background after:bg-gradient-to-l after:top-0 after:right-0 after:bottom-0 after:w-20 after:z-10 before:absolute before:content[""] before:to-transparent before:from-background before:bg-gradient-to-r before:top-0 before:left-0 before:bottom-0 before:w-20 before:z-10'>
+          {[...Array(2)].map((arr, index) => (
+            <div key={index} className="flex flex-nowrap animate-slide">
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className="relative w-[200px] m-20 flex items-center shrink-0">
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="relative px-4 sm:px-6 flex justify-center items-center flex-col">
+        <div className="absolute top-24 w-1/3 h-32 blur-[120px] rounded-full bg-secondary -z-10" />
+        <TitleSection
+          title="Keep track of your meetings all in one place"
+          subheading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
+          pill="Features"
+        />
+        {/* <div
+          className="mt-10
+          max-w-[450px]
+          flex
+          justify-center
+          items-center
+          relative
+          sm:ml-0
+          rounded-2xl
+          border-8
+          border-washed-purple-300 
+          border-opacity-10
+        ">
+          <Image src={Cal} alt="Banner" className="rounded-2xl" />
+        </div> */}
+      </section>
+      {/* <section className="relative">
+        <div
+          className="w-full
+          blur-[120px]
+          rounded-full
+          h-32
+          absolute
+          bg-brand-primaryPurple/50
+          -z-100
+          top-56
+        "
+        />
+        <div
+          className="mt-20
+          px-4
+          sm:px-6 
+          flex
+          flex-col
+          overflow-x-hidden
+          overflow-visible
+        ">
+          <TitleSection
+            title="Trusted by all"
+            subheading="Join thousands of satisfied users who rely on our platform for their 
+            personal and professional productivity needs."
+            pill="Testimonials"
+          />
+          {[...Array(2)].map((arr, index) => (
+            <div
+              key={randomUUID()}
+              className={twMerge(
+                clsx('mt-10 flex flex-nowrap gap-6 self-start', {
+                  'flex-row-reverse': index === 1,
+                  'animate-[slide_250s_linear_infinite]': true,
+                  'animate-[slide_250s_linear_infinite_reverse]': index === 1,
+                  'ml-[100vw]': index === 1,
+                }),
+                'hover:paused'
+              )}>
+              {USERS.map((testimonial, index) => (
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px]
+                  shrink-0s
+                  rounded-xl
+                  dark:bg-gradient-to-t
+                  dark:from-border dark:to-background
+                "
+                  cardHeader={
+                    <div
+                      className="flex
+                      items-center
+                      gap-4
+                  ">
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLocaleLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }></CustomCard>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section> */}
+      {/* <section
+        className="mt-20
+        px-4
+        sm:px-6
+      ">
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div
+          className="flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        ">
+          {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={card.planType}
+              className={clsx(
+                'w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative',
+                {
+                  'border-brand-primaryPurple/70':
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className="text-2xl
+                  font-semibold
+              ">
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div
+                        className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/80
+                        -z-10
+                        top-0
+                      "
+                      />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span
+                    className="font-normal 
+                    text-2xl
+                ">
+                    ${card.price}
+                  </span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ''
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button
+                    variant="btn-primary"
+                    className="whitespace-nowrap w-full mt-4">
+                    {card.planType === PRICING_PLANS.proplan
+                      ? 'Go Pro'
+                      : 'Get Started'}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul
+                  className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                ">
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li
+                      key={feature}
+                      className="flex
+                      items-center
+                      gap-2
+                    ">
+                      <Image src={CheckIcon} alt="Check Icon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
+          ))}
+        </div>
+      </section> */}
+    </>
   )
 }
--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -10,8 +10,8 @@ export default function TitleSection({
 }: TitleSectionProps) {
   return (
     <>
-      <section className="flex flex-col gap-4 justify-center items-start md:items-center">
-        <article className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-sky-600 dark:to-purple-800">
+      <div className="flex flex-col gap-4 justify-center items-start md:items-center">
+        <article className="rounded-full p-[1.2px] text-sm bg-gradient-to-r from-primary to-secondary">
           <div className="rounded-full px-3 py-1 bg-background ">{pill}</div>
         </article>
         {subheading ? (
@@ -19,7 +19,7 @@ export default function TitleSection({
             <h2 className="text-left text-3xl sm:text-5xl sm:max-w-[750px] md:text-center font-semibold">
               {title}
             </h2>
-            <p className="dark:text-purple-300 sm:max-w-[450px] md:text-center">
+            <p className="text-accent-foreground sm:max-w-[450px] md:text-center">
               {subheading}
             </p>
           </>
@@ -28,7 +28,7 @@ export default function TitleSection({
             {title}
           </h1>
         )}
-      </section>
+      </div>
     </>
   )
 }
